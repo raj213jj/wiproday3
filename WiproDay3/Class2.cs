1e137bf988f48f3a81c85379014ab3ea111be9f8
raj213jj <harirajmyaka@gmail.com>
2025-01-29 12:27:59 +0530

    modified

---
+++ WiproDay3/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiproDay3
+{
+    public class Customer
+    {
+        public int custid;
+        public string custname;
+
+
+        public void Display()
+        {
+            Console.WriteLine($"Customerid :{custid} \nCustomername : {custname}");
+
+        }
+    }
+}
