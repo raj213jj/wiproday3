7e2309b082a6d1b93d06dd2b7e2ad519e0960828
raj213jj <harirajmyaka@gmail.com>
2025-01-29 12:19:21 +0530

    mofified

--- WiproDay3/Program.cs
+++ WiproDay3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace WiproDay3
+{
+     class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the ProductId");
+            int id=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the ProductName");
+            String nm=Console.ReadLine();
+            
+
+            Product pr=new Product(id,nm);
+            pr.PublicMthd();
+
+            Customer cs=new Customer();
+            Console.WriteLine("enter the customerid");
+            cs.custid= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the customername");
+            cs.custname = Console.ReadLine();
+            cs.Display();
+
+
+        }
+    }
+}
