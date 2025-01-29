39b6253b8f963ab4d901c2d8a8efe2fcd0f819c3
raj213jj <harirajmyaka@gmail.com>
2025-01-29 12:13:40 +0530

    changedfrominternaltopublic

---
+++ WiproDay3/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiproDay3
+{
+    class Product
+    {
+        int Proid;
+        String ProName;
+
+
+        public Product(int id, String name)
+        {
+            Proid = id;
+            ProName = name;
+        }
+    
+
+       
+        public void PublicMthd()
+        {
+            Console.WriteLine("Public Method" + Proid + " " + ProName);
+        }
+    }
+
+
+}
+
